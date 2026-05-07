f4365142edda6598109dfb5ec861c352dbaa13f7
Brayden <b.0523leasure@gmail.com>
2026-05-07 20:48:33 +0000

    Added functionality to web app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
+scan_results = [
+    {
+        "severity": "HIGH",
+        "package": "openssl",
+        "cve": "CVE-2025-1234"
+    },
+    {
+        "severity": "MEDIUM",
+        "package": "flask",
+        "cve": "CVE-2025-5678"
+    }
+]
+
 @app.route("/")
 def home():
-    return "Secure DevSecOps Lab Running"
+    return render_template(
+        "index.html",
+        scan_results=scan_results,
+        timestamp=datetime.now()
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
